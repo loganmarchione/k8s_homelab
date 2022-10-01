14f2b6d0b9a1d6ad5191fa767d5254c1584a443d
Logan Marchione <logan@loganmarchione.com>
2022-10-01 11:09:16 -0400

    More changes

--- bootstrap/setupMasterNode.sh
+++ bootstrap/setupMasterNode.sh
@@ -6,29 +6,73 @@ set -u
 set -o pipefail
 IFS=$'\n\t'
 
+# Set generic variables
+hostname=$(uname -n)
+
 message() {
-  echo -e "\n######################################################################"
+  echo "######################################################################"
   echo "# $1"
   echo "######################################################################"
 }
 
+userCheck() {
+  # Fail if running as root
+  if [[ $(id -u) -eq 0 ]]; then
+    message "ERROR: Can't run as root" >> "$logfile"
+    exit 1
+  else
+    message "STATE: Running as $(id -u -n)\n"
+  fi
+
+}
+
 installPackages() {
-  message "running installPackages"
+  message "STATE: running installPackages"
 
   sudo apt update && sudo apt upgrade -y
 
-  sudo apt install -y curl wget
+  sudo apt install -y curl
+
+  sudo apt autoremove -y
 }
 
 installK3s() {
-  message "running installK3s"
 
-  curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server  --cluster-init  --token $K3S_TOKEN --tls-san k8s-0-cp --tls-san k8s-1-cp --tls-san k8s-2-cp-hp7 --tls-san rancher.rsr.net --disable servicelb --disable traefik --disable local-storage --flannel-backend=host-gw --node-label k3s-upgrade=enabled " sh -
-  curl -sfL https://get.k3s.io | sh -s - server --disable servicelb
+  message "STATE: Detecting K3s...\n"
+  if [[ -x "$(command -v /usr/local/bin/k3s)" ]]; then
+    read -p "Continue (y/n)?" CONT
+    if [ "$CONT" = "y" ]; then
+      echo "yaaa";
+      exit 1
+      #   message "running installK3s"
+      #   curl -sfL https://get.k3s.io | sh -s - server --cluster-init --tls-san $hostname --disable servicelb
+      #   sleep 10
+    else
+      echo "booo";
+      exit 1
+    fi
 
+  message "STATE: Getting cluster info"
+  kubectl cluster-info
+
+  message "STATE: Getting nodes"
   kubectl get nodes -o wide
+
+  message "STATE: Contents of your kubeconfig file are below (copy/paste this for later)\n"
+  cat /etc/rancher/k3s/k3s.yaml
+
+  if [[ -x "$(command -v ufw)" ]]; then
+    message "STATE: UFW is installed, opening 6443/tcp\n"
+    sudo ufw allow 6443/tcp
+    sudo ufw reload
+  else
+    message "ERROR: UFW was not found, please make sure 6443/tcp is open\n"
+  fi
 }
 
+
+userCheck
 installPackages
+#installK3s 
 
 message "STATE: Completed"
